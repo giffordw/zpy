f3841380e0a3e4ac0276ebe42a6034a454c4f422
giffordw <dgifford19@gmail.com>
2015-08-03 23:07:09 -0400

    Major changes reflecting Anthonys additions. Still testing

--- zpy/__init__.py
+++ zpy/__init__.py
@@ -5,6 +5,8 @@ from scipy.stats import pearsonr
 from scipy.stats import norm
 import scipy.signal as signal
 from matplotlib import gridspec
+import matplotlib
+matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
 from matplotlib.widgets import RadioButtons, Button, CheckButtons
 import sys
@@ -45,7 +47,7 @@ class Estimateline:
         else: return
 
 class z_est:
-    def __init__(self,lower_w=3900.0,upper_w=5500.0,lower_z=0.01,upper_z=0.35,z_res=3.0e-5):
+    def __init__(self,lower_w=3900.0,upper_w=5500.0,lower_z=0.01,upper_z=0.35,z_res=3.0e-5,skip_initial_priors=False):
         '''
         Initialize redshift estimate parameters
         '''
@@ -70,11 +72,17 @@ class z_est:
         #create redshift array and initialize correlation value array
         self.ztest = np.arange(self.lower_z,self.upper_z,self.z_res)
         self.corr_val_i = np.zeros(self.ztest.size)
-
+        
         #set redshift prior flag
-        self.est_pre_z = raw_input('(1) Use a known prior [Examples: median of known redshifts. Galaxy photoz measurements] \n'\
-                            '(2) View spectrum and specify a redshift prior \n'\
-                            '(3) No prior\n')
+        if skip_initial_priors:
+            self.est_pre_z = '3'
+            self.uline_n = 'HK'
+            self.uline = 3950.0
+            self.z_prior_width = 0.06
+        else:
+            self.est_pre_z = raw_input('(1) Use a known prior [Examples: median of known redshifts. Galaxy photoz measurements] \n'\
+                                    '(2) View spectrum and specify a redshift prior \n'\
+                                    '(3) No prior\n')
 
         #catch and correct false entry
         _est_enter = False
@@ -114,12 +122,14 @@ class z_est:
                          "Incorrect string value for prior"
 
 
-    def redshift_estimate(self,early_type_wave,early_type_flux,wave,Flux_science,gal_prior=None):
+    def redshift_estimate(self,early_type_wave,early_type_flux,wave,Flux_science,plotlines=None,template_id='Galaxy',gal_prior=None):
         '''
         estimate redshift for object
         '''
         #manage redshift prior
         self.gal_prior = gal_prior
+        self.template_number = template_id
+        self.plotlines = plotlines
 
         #continuum subtract
         Flux_sc = Flux_science - signal.medfilt(Flux_science,171)
@@ -156,8 +166,9 @@ class z_est:
 
         redshift_est,cor,ztest,corr_val = self._cross_cor(self.pre_z_est,self.z_prior_width,early_type_wave,early_type_flux_sc,wave,Flux_sc)
 
-        self.qualityval = 0
+        self.qualityval = 1
         self.first_pass = True
+        self.skip_spec_flag = False
         self._GUI_display(redshift_est,ztest,corr_val,wave,Flux_science)
         #self.line_est = Estimateline(self.pspec,ax)
         #plt.show()
@@ -168,8 +179,7 @@ class z_est:
             redshift_est,cor,ztest,corr_val = self._cross_cor(self.pre_z_est,self.z_prior_width,early_type_wave,early_type_flux_sc,wave,Flux_sc)
             print 'redshift est:',redshift_est
             self._GUI_display(redshift_est,ztest,corr_val,wave,Flux_science)
-            total_new_shift = self.spectra2.dx_tot
-            redshift_est = (self.uline*(1+redshift_est) - total_new_shift)/self.uline - 1
+            redshift_est = self.spectra2.finalz
         except AttributeError:
             pass
         return redshift_est,cor,ztest,corr_val,self.qualityval
@@ -230,47 +240,85 @@ class z_est:
         gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
         ax2 = plt.subplot(gs[0])
         ax = plt.subplot(gs[1])
-        plt.subplots_adjust(right=0.8)
+
+        #maximize window
+        figManager = plt.get_current_fig_manager()
+        figManager.window.showMaximized()
+        
+        plt.subplots_adjust(top=0.96,bottom=0.1,left=0.04,right=0.80)
 
         ax.plot(ztest,corr_val,'b')
-        ax.axvline(redshift_est,color='k',ls='--')
+        pspec_corr = ax.axvline(redshift_est,color='k',ls='--')
         ax.fill_between(self.ztest,np.zeros(self.ztest.size),self.corr_prior,facecolor='grey',alpha=0.6)
         ax.set_xlabel('Redshift')
         ax.set_ylabel('Correlation')
 
         self.pspec, = ax2.plot(wave,flux_sc)
-        ax2.axvline(3725.0*(1+redshift_est),ls='--',alpha=0.7,c='blue')
-        ax2.axvline(3968.5*(1+redshift_est),ls='--',alpha=0.7,c='red')
-        ax2.axvline(3933.7*(1+redshift_est),ls='--',alpha=0.7,c='red')
-        ax2.axvline(4102.9*(1+redshift_est),ls='--',alpha=0.7,c='orange')
-        ax2.axvline(4304.0*(1+redshift_est),ls='--',alpha=0.7,c='orange')
-        ax2.axvline(4862.0*(1+redshift_est),ls='--',alpha=0.7,c='orange')
-        ax2.axvline(4959.0*(1+redshift_est),ls='--',alpha=0.7,c='blue')
-        ax2.axvline(5007.0*(1+redshift_est),ls='--',alpha=0.7,c='blue')
-        ax2.axvline(5175.0*(1+redshift_est),ls='--',alpha=0.7,c='orange')
+        ax2.set_ylim(np.min(flux_sc),np.max(flux_sc))
+        ax2.set_xlim(wave[0],wave[-1])
+
+        #plot lines
+        vlin_pspecs = []
+        if self.plotlines == None:
+            self.plotlines = {'HKlines':[3968.5,3933.7],'emission_lines':[3725.0,4959.0,5007.0],'absorption_lines':[4102.9,4304.0,4862.0,5175.0]}
+        else: pass
+        try:
+            for vlin in self.plotlines['HKlines']:
+                vlin_pspecs.append(ax2.axvline(vlin*(1+redshift_est),ls='--',alpha=0.7,c='red'))
+        except KeyError: pass
+        try:
+            for vlin in self.plotlines['emission_lines']:
+                vlin_pspecs.append(ax2.axvline(vlin*(1+redshift_est),ls='--',alpha=0.7,c='blue'))
+        except KeyError: pass
+        try:
+            for vlin in self.plotlines['absorption_lines']:
+                vlin_pspecs.append(ax2.axvline(vlin*(1+redshift_est),ls='--',alpha=0.7,c='orange'))
+        except KeyError: pass
+
+            
         if self.first_pass:
             self.line_est = Estimateline(self.pspec,ax2,self.uline_n)
-        rax = plt.axes([0.85, 0.5, 0.1, 0.2])
-        if self.qualityval == 0:
-            radio = RadioButtons(rax, ('Unclear', 'Clear'))
+        
+        self.fig.text(0.83, 0.9, 'Template {0}'.format(self.template_number), bbox=dict(facecolor='white', alpha=1.),fontsize=18)
+        
+        rax = plt.axes([0.83, 0.43, 0.15, 0.22])
+        if self.qualityval == 1:
+            radio = RadioButtons(rax, ('1 - No Clue      ','2 - Slight\n    Chance', '3 - Maybe', '4 - Probably', '5 - Clear'))
         else:
-            radio = RadioButtons(rax, ('Unclear', 'Clear'),active=1)
+            radio = RadioButtons(rax, ('1 - No Clue      ','2 - Slight\n    Chance', '3 - Maybe', '4 - Probably', '5 - Clear'),active=1)
         def qualfunc(label):
-            if label == 'Clear':
-                self.qualityval = 1
+            if label == '5 - Clear':
+                self.qualityval = 5
+            elif label == '4 - Probably':
+                self.qualityval = 4
+            elif label == '3 - Maybe':
+                self.qualityval = 3
+            elif label == '2 - Slight\n    Chance':
+                self.qualityval = 2
             else:
-                self.qualityval = 0
+                self.qualityval = 1
+
         radio.on_clicked(qualfunc)
         closeax = plt.axes([0.83, 0.3, 0.15, 0.1])
         button = Button(closeax, 'Accept & Close', hovercolor='0.975')
         def closeplot(event):
             plt.close()
         button.on_clicked(closeplot)
+        skip_spec_ax = plt.axes([0.83, 0.94, 0.15, 0.04])
+        skip_button = Button(skip_spec_ax, 'skip spectra', hovercolor='0.975')
+        def skip_spec(event):
+            plt.close()
+            self.qualityval = 0
+            self.skip_spec_flag = True
+        skip_button.on_clicked(skip_spec)
         ax2.set_xlim(self.lower_w,self.upper_w)
         ax2.set_xlabel('Wavelength (A)')
         ax2.set_ylabel('Counts')
+
+        plt.draw()
+
         if not self.first_pass:
-            self.spectra2 = DragSpectra(self.pspec,flux_sc,ax2,self.fig)
+            self.spectra2 = DragSpectra(vlin_pspecs,pspec_corr,redshift_est,ax2)
             self.fig.canvas.mpl_connect('motion_notify_event',self.spectra2.on_motion)
             self.fig.canvas.mpl_connect('button_press_event',self.spectra2.on_press)
             self.fig.canvas.mpl_connect('button_release_event',self.spectra2.on_release)
@@ -278,40 +326,50 @@ class z_est:
 
 class DragSpectra:
     '''Class to drage the spectra back and forth to match lines of interest'''
-    def __init__(self,spectra,ydata,ax5,fig):
+    def __init__(self,vlin_spectra,corr_spec,redshift_estimate,ax5):
         self.ax5 = ax5
-        self.fig = fig
-        self.tb = plt.get_current_fig_manager().toolbar
+        self.corr_spec = corr_spec
+        self.yzs = self.corr_spec.get_data()[1]
         print 'begin shift'
-        self.spectra = spectra
-        self.ydata = ydata
+        self.vlin_spectra = vlin_spectra
+        self.vline_ys = vlin_spectra[0].get_data()[1]
         self.pressed = False
-        self.dx_tot = 0.0
+        self.finalz = redshift_estimate
         #figure.canvas.mpl_connect('motion_notify_event',self.on_motion)
         #figure.canvas.mpl_connect('button_press_event',self.on_press)
         #figure.canvas.mpl_connect('button_release_event',self.on_release)
 
     def on_motion(self,evt):
         if self.pressed:
-            dx = evt.xdata - self.mouse_x
-            self.spectra.set_data(self.spectra_x + dx,self.ydata)
+            #dx = evt.xdata - self.mouse_x 
+            #print "%d %d" % (evt.xdata,self.mouse_x)
+            newz = ((evt.xdata/self.mouse_x)*(1.+self.z_on_press))-1.  #((1. + (dx/self.mouse_x))*(1.+self.z0))-1.   
+            newxs = self.vline_lams*(evt.xdata/self.mouse_x) # equivalent to spec_x*((1+newz)/(1+z0))
+            for i in np.arange(len(self.vlin_spectra)):
+                self.vlin_spectra[i].set_data([newxs[i], newxs[i]], self.vline_ys) 
+                                        
+            self.corr_spec.set_data([newz, newz], self.yzs)
             plt.draw()
 
+
     def on_press(self,evt):
-        if evt.inaxes == self.ax5 and self.tb.mode == '':
+        if evt.inaxes == self.ax5:
             self.mouse_x = evt.xdata
-            self.spectra_x = self.spectra.get_xdata()
+            self.z_on_press = self.corr_spec.get_data()[0][0]
+            self.vline_lams = np.array([self.vlin_spectra[x].get_data()[0][0] for x in np.arange(len(self.vlin_spectra))])
             self.pressed = True
         else: return
 
     def on_release(self,evt):
-        if evt.inaxes == self.ax5 and self.tb.mode == '':
+        if evt.inaxes == self.ax5:
             self.pressed = False
             try:
-                self.dx_tot += evt.xdata - self.mouse_x
+                self.finalz = self.corr_spec.get_data()[0][0]
             except AttributeError:
-                self.dx_tot = 0.0
+                self.finalz = self.finalz
         else: return
+
+
 
 if __name__ == '__main__':
     R = z_est()
